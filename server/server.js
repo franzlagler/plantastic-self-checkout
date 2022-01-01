da07e2e4c8529e3ef15f1a2825926ce8803bc0d7
Franz Lagler <franzgabriellagler@Franzs-MacBook-Pro.local>
2022-01-01 23:03:02 +0100

    Update .gitignore

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("dotenv").config({ path: "../config.env" });
+require("dotenv").config({ path: "./config.env" });
 const db = require("./db/connect");
 
 const port = process.env.PORT || 5000;

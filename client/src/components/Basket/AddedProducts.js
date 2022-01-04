b308163bbe4007454763bd5dc1ecde01a119de3b
franzlagler <franz.lagler@outlook.com>
2022-01-04 11:36:24 +0100

    Resize Scanner

--- client/src/components/Basket/AddedProducts.js
+++ client/src/components/Basket/AddedProducts.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { BoldText, Paragraph, DeleteButton } from "../Miscellaneous";
 import { MobileSingleProduct, RegularSingleProduct } from "./SingleProduct";
 
 const AddedProductsContainer = styled.div`
